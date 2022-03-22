f8b29b750ae8db177449e329320eaa8bc0f8a654
Sandhyaa2904 <102170769+Sandhyaa2904@users.noreply.github.com>
2022-03-22 21:18:04 +0530

    Update sans.py

--- sans.py
+++ sans.py
@@ -4,22 +4,42 @@ app=Flask(__name__)
 @app.get('/')
 def hi():
     return "good night"
-
-@app.post('/r')
+@app.post('/h')
 def hi1():
-    con = sqlite3.Connection("C:/Users/trc/Desktop/mycode/task/Scripts/task.db")
+    con = sqlite3.Connection("C:/Users/trc/Desktop/mycode/task/Scripts/task.db") 
     cur=con.cursor()
     data = request.get_json()
     Name=data["Name"]
     Rollno=data["Rollno"]
     Mark=data["Mark"]
-    students=(Name,Rollno,Mark)
-    cur.execute("create table if not exists students(name varchar(255),Rollno varchar(255),Mark int)")
-    cur.execute("insert into students values(?,?,?)",students)
+    student=(Name,Rollno,Mark)
+    cur.execute("create table if not exists student(name varchar(255),Rollno varchar(255),mark int)")
+    cur.execute("insert into student values(?,?,?)",student)
     con.commit()
     con.close()
     print(data)
-    return "we got the data"
+    return "data collected"
+@app.patch('/pat/<inputmark>')
+def patchmethod(inputmark):
+    data = request.get_json()
+    users = data
+    if inputmark in users.values():
+        users["Mark"] = 99
+        res = "Data updated"
+        return res
+    print(f"The data after creation is {users}")
+    res = "Data created"
+@app.delete("/delete/<rollno>")
 
-
+def deletes(rollno):
+    
+    delete(rollno);
+    return("deleted");
+def delete(rollno):
+    con = sqlite3.Connection("C:/Users/trc/Desktop/mycode/task/Scripts/task.db") 
+    query = f'delete from student where rollno = "{rollno}"';
+    cur = con.cursor();
+    cur.execute(query)
+    con.commit()
+  
 app.run(debug=True)
